58b880859d6e93d6e968a5b244a7940d50a94ad2
ME <mebener44@gmail.com>
2021-04-24 15:13:20 -0700

    code snapshop cleanup

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,9 +1,8 @@
-var languageButtonsEl = document.querySelector("#language-buttons");
 var userFormEl = document.querySelector("#user-form");
+var languageButtonsEl = document.querySelector("#language-buttons");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
-
 
 var formSubmitHandler = function(event) {
   // prevent page from refreshing
@@ -23,32 +22,17 @@ var formSubmitHandler = function(event) {
   }
 };
 
-var getFeaturedRepos = function(language) {
-    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
-  
-    fetch(apiUrl).then(function(response) {
-      if (response.ok) {
-        response.json().then(function(data) {
-          console.log(data)
-        });
-      } else {
-        alert('Error: GitHub User Not Found');
-      }
-    });
-  };
+var buttonClickHandler = function(event) {
+  // get the language attribute from the clicked element
+  var language = event.target.getAttribute("data-language");
 
-  var buttonClickHandler = function(event){
-      var language = event.target.getAttribute("data-language");
+  if (language) {
+    getFeaturedRepos(language);
 
-      if (language) {
-        getFeaturedRepos(language);
-
-        //clear old content
-        repoContainerEl.textContent = "";
-      }
-
-    
-}
+    // clear old content
+    repoContainerEl.textContent = "";
+  }
+};
 
 var getUserRepos = function(user) {
   // format the github api url
@@ -71,6 +55,23 @@ var getUserRepos = function(user) {
     .catch(function(error) {
       alert("Unable to connect to GitHub");
     });
+};
+
+var getFeaturedRepos = function(language) {
+  // format the github api url
+  var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+  // make a get request to url
+  fetch(apiUrl).then(function(response) {
+    // request was successful
+    if (response.ok) {
+      response.json().then(function(data) {
+        displayRepos(data.items, language);
+      });
+    } else {
+      alert("Error: " + response.statusText);
+    }
+  });
 };
 
 var displayRepos = function(repos, searchTerm) {
@@ -119,11 +120,6 @@ var displayRepos = function(repos, searchTerm) {
   }
 };
 
-
-
-
-// add event listeners to forms
+// add event listeners to form and button container
 userFormEl.addEventListener("submit", formSubmitHandler);
-
 languageButtonsEl.addEventListener("click", buttonClickHandler);
-
